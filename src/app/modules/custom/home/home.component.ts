61d450b986392d5ba57f864b49f9ef85348767d1
Adicom.ir <adicom.ir@gmail.com>
2019-02-19 23:39:23 +0330

    add filter logic, complete convert

--- src/app/modules/custom/home/home.component.ts
+++ src/app/modules/custom/home/home.component.ts
@@ -21,6 +21,15 @@ export class HomeComponent implements OnInit {
     newMidRate = 1;
     intNewMidRate = 1;
     floatNewMidRate = 0;
+    filter = [100, 100, 100];
+    spiritItems = [
+        [72, 86, 93, 100, 107, 114, 121, 128, 135, 142], // normal
+        [81, 97, 105, 113, 122, 130, 138, 146, 154, 162], // mots
+        [63, 75, 81, 87, 92, 98, 104, 110, 116, 122] // pic
+    ];
+    oldSpirit = 0;
+    newSpirit = 0;
+    negaresh = 1;
 
     constructor(private router: Router) { }
 
@@ -34,7 +43,7 @@ export class HomeComponent implements OnInit {
         let intValue = Math.floor(value);
         let floatValue = value - intValue;
         floatValue = floatValue * 4;
-        intValue = intValue * 4 - 3 + floatValue;        
+        intValue = intValue * 4 - 3 + floatValue;
 
         switch (pos) {
             case 'LD':
@@ -68,9 +77,11 @@ export class HomeComponent implements OnInit {
         this.defRate = (this.rateValues[0] + this.rateValues[1] + this.rateValues[2]);
         this.midRate = (this.rateValues[3] * 3);
         this.midDefaultRate = this.midRate;
-        // mid = mid * filter[0] / 100;
-        // mid = mid * filter[1] / 100;
-        // mid = mid * filter[2] / 100;
+
+        this.midRate = this.midRate * this.filter[0] / 100;
+        this.midRate = this.midRate * this.filter[1] / 100;
+        this.midRate = this.midRate * this.filter[2] / 100;
+
         this.midRate = Math.floor(this.midRate);
         this.midDefaultRate -= this.midRate;
         this.midDefaultRate *= -1;
@@ -82,21 +93,63 @@ export class HomeComponent implements OnInit {
         this.newMidRate = Math.round(this.midRate / 3);
         this.intNewMidRate = Math.floor((this.newMidRate + 3) / 4);
         this.floatNewMidRate = ((this.newMidRate + 3) / 4) - this.intNewMidRate;
-        
+
         // var color = 'black';
         // if (this.rateValues[3] < arr3) color = 'green';
         // if (this.rateValues[3] > arr3) color = 'red';
-        // $('#mDiff').val(intNewMidRate + floatNewMidRate);
         // $('#mDiff').css('color', color);
-    
-        // // Average Rating
-        // $('#ave_def').html('Ave Defence: ' + Math.round(def / 3));
-        // $('#ave_mid').html('Ave Midfield: ' + Math.round(mid / 3));
-        // $('#ave_att').html('Ave Attack: ' + Math.round(att / 3));
-        // $('#ave_total').html('Ave Total: ' + Math.round((def / 3 + mid / 3 + att / 3) / 3));
     }
-    
+
     getAverage(value) {
         return Math.round(value / 3);
+    }
+
+    filterClick(type, value) {
+        if (type == 1) {
+            switch (value) {
+                case 1:
+                    this.changeNegaresh(1);
+                    break;
+                case 2:
+                    this.changeNegaresh(2);
+                    break;
+                case 3:
+                    this.changeNegaresh(3);
+                    break;
+            }
+        } else if (type == 2) {
+            switch (value) {
+                case 1:
+                    this.filter[1] = 100;
+                    break;
+                case 2:
+                    this.filter[1] = 119.892;
+                    break;
+                case 3:
+                    this.filter[1] = 111.493;
+                    break;
+            }
+        } else if (type == 3) {
+            switch (value) {
+                case 1:
+                    this.filter[2] = 100;
+                    break;
+                case 2:
+                    this.filter[2] = 93;
+                    break;
+            }
+        }
+        this.calculateRating();
+    }
+
+    changeNegaresh(value) {
+        this.negaresh = value;
+        let temp = this.spiritItems[value - 1][this.newSpirit] - this.spiritItems[0][this.oldSpirit];
+        this.filter[0] = 100 + temp;
+    }
+
+    changeSpirit() {
+        this.changeNegaresh(this.negaresh);
+        this.calculateRating();
     }
 }
