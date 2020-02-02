7e3d5c55f31269d732a1d0ba6661086cef6b8c2e
adicomdotir <adicom.ir@gmail.com>
2020-02-03 00:40:55 +0330

    Add position detail with save

--- src/app/modules/custom/position-detail/position-detail.component.ts
+++ src/app/modules/custom/position-detail/position-detail.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Player } from 'src/app/shared/models/player';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-position-detail',
-  templateUrl: './position-detail.component.html',
-  styleUrls: ['./position-detail.component.css']
+    selector: 'app-position-detail',
+    templateUrl: './position-detail.component.html',
+    styleUrls: ['./position-detail.component.css']
 })
 export class PositionDetailComponent implements OnInit {
+    index = -1;
+    player: Player;
+    players: Player[];
 
-  constructor() { }
+    constructor(private route: ActivatedRoute, private location: Location) {
+        this.route.paramMap.subscribe(params => {
+            this.index = Number(params['params'].id) - 1;
+        });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.players = JSON.parse(localStorage.getItem('POSITION'));
+        this.player = this.players[this.index];
+    }
 
+    savePlayer() {
+        this.players[this.index] = this.player;
+        localStorage.setItem('POSITION', JSON.stringify(this.players));
+        this.location.back();
+    }
 }
