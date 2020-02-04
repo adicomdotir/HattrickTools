9b9610306ba1bb9af54422ce5e8abc6f550c86f3
adicomdotir <adicom.ir@gmail.com>
2020-02-04 12:11:59 +0330

    WP: Modify ui and ux

--- src/app/modules/custom/position-detail/position-detail.component.ts
+++ src/app/modules/custom/position-detail/position-detail.component.ts
@@ -22,12 +22,17 @@ export class PositionDetailComponent implements OnInit {
     ngOnInit() {
         this.players = JSON.parse(localStorage.getItem('POSITION'));
         this.player = this.players[this.index];
+        console.log(this.player)
     }
 
     savePlayer() {
         console.log(this.player)
         this.players[this.index] = this.player;
         localStorage.setItem('POSITION', JSON.stringify(this.players));
+        this.location.back();
+    }
+
+    cancelBtn() {
         this.location.back();
     }
 }
